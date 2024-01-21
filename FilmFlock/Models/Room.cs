a7afe496be2738fab9f2f926bcbd6f84621288dc
Myles Barros <myles.barros@sap.com>
2024-01-21 17:44:49 +0100

    [Models] Restore Room Users Setter

--- FilmFlock/Models/Room.cs
+++ FilmFlock/Models/Room.cs
@@ -12,7 +12,7 @@ public class Room
     public Guid AdminId { get; }
     public FilmSelectionMethod FilmSelectionMethod { get; }
     public ushort PerUserFilmLimit { get; }
-    public List<User> Users { get; }
+    public List<User> Users { get; set; }
 
     public Room(
         string roomId,
@@ -48,48 +48,4 @@ public class Room
         return Users.ToArray();
     }
 
-}
-
-public interface IRoomIdGenerator
-{
-    public string GenerateNew();
-}
-
-public class RoomIdGenerator : IRoomIdGenerator
-{
-    // We've removed ambiguous characters like 0, O, 1, and I.
-    private static char[] RoomIdCharacters = {
-       '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
-    };
-
-    private IRoomStorage RoomStorage;
-
-    public RoomIdGenerator(IRoomStorage roomStorage)
-    {
-        RoomStorage = roomStorage;
-    }
-
-    public string GenerateNew()
-    {
-        char tumbler1 = RandomCharacter();
-        char tumbler2 = RandomCharacter();
-        char tumbler3 = RandomCharacter();
-        char tumbler4 = RandomCharacter();
-        char tumbler5 = RandomCharacter();
-
-        string proposedId = "{tumbler1}{tumbler2}{tumbler3}{tumbler4}{tumbler5}";
-        Room? existingRoom = RoomStorage.GetRoom(proposedId);
-        if (existingRoom == null)
-            return proposedId;
-        
-        return GenerateNew();
-    }
-
-    private char RandomCharacter()
-    {
-        Random random = new Random();
-        int randomIndex = random.Next(0, RoomIdGenerator.RoomIdCharacters.Length);
-
-        return RoomIdCharacters[randomIndex];
-    }
 }
