e396148f932f073b0a6bfe3271e9d8ffd201a365
Myles Barros <myles.barros@sap.com>
2024-01-21 17:43:01 +0100

    [Models] Truncate Room IDs to 4 Characters

--- FilmFlock/Models/Room.cs
+++ FilmFlock/Models/Room.cs
@@ -1,9 +1,10 @@
 using System.Text.Json.Serialization;
 using System.Xml;
+using Microsoft.Extensions.ObjectPool;
+using Microsoft.VisualBasic;
 
 namespace FilmFlock.Models;
 
-[Serializable]
 public class Room
 {
     public string RoomId { get; }
@@ -11,7 +12,7 @@ public class Room
     public Guid AdminId { get; }
     public FilmSelectionMethod FilmSelectionMethod { get; }
     public ushort PerUserFilmLimit { get; }
-    public List<User> Users { get; set; }
+    public List<User> Users { get; }
 
     public Room(
         string roomId,
@@ -30,9 +31,9 @@ public class Room
         Users = new List<User>(users);
     }
 
-    public Room(FilmSelectionMethod selectionMethod, ushort perUserFilmLimit)
+    public Room(string roomId, FilmSelectionMethod selectionMethod, ushort perUserFilmLimit)
     : this(
-        System.Guid.NewGuid().ToString(),
+        roomId,
         DateTime.UtcNow,
         System.Guid.NewGuid(),
         selectionMethod,
@@ -47,4 +48,48 @@ public class Room
         return Users.ToArray();
     }
 
+}
+
+public interface IRoomIdGenerator
+{
+    public string GenerateNew();
+}
+
+public class RoomIdGenerator : IRoomIdGenerator
+{
+    // We've removed ambiguous characters like 0, O, 1, and I.
+    private static char[] RoomIdCharacters = {
+       '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+    };
+
+    private IRoomStorage RoomStorage;
+
+    public RoomIdGenerator(IRoomStorage roomStorage)
+    {
+        RoomStorage = roomStorage;
+    }
+
+    public string GenerateNew()
+    {
+        char tumbler1 = RandomCharacter();
+        char tumbler2 = RandomCharacter();
+        char tumbler3 = RandomCharacter();
+        char tumbler4 = RandomCharacter();
+        char tumbler5 = RandomCharacter();
+
+        string proposedId = "{tumbler1}{tumbler2}{tumbler3}{tumbler4}{tumbler5}";
+        Room? existingRoom = RoomStorage.GetRoom(proposedId);
+        if (existingRoom == null)
+            return proposedId;
+        
+        return GenerateNew();
+    }
+
+    private char RandomCharacter()
+    {
+        Random random = new Random();
+        int randomIndex = random.Next(0, RoomIdGenerator.RoomIdCharacters.Length);
+
+        return RoomIdCharacters[randomIndex];
+    }
 }
